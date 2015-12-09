5cf6f9c01f0a72c84da651297ee9cacefa621507
agannyagm <agannya@outlook.com>
2015-12-09 13:50:50 -0800

    minor changes

--- js/gallery.js
+++ js/gallery.js
@@ -55,8 +55,9 @@ $(document).ready(function () {
     $('.hamburger').click(function () {
 
         $('nav').slideToggle(function () {
-            $('nav li').css('display', 'inline-block');
+            $('nav li').css('display', 'inline');
             $('nav').css('float', 'right');
+            $('nav ul').css('float', 'right');
             $('nav').css('padding', '0em');
         })
 
--- recipe/js/index.js
+++ recipe/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.hamburger').click(function () {
 
         $('nav').slideToggle(function () {
-            $('nav li').css('display', 'inline-block');
+            $('nav li').css('display', 'inline');
             $('nav').css('float', 'right');
             $('nav ul').css('float', 'right');
             $('nav').css('padding', '0em');
